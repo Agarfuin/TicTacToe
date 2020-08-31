46783dd2fa22156ce1ecd14ddfb0da450c1a55e4
Harun Selman Karakas <mail@harunkarakas.com>
2020-08-31 23:31:56 +0300

    Final changes, game stable

--- tictactoe.py
+++ tictactoe.py
@@ -5,75 +5,75 @@ theBoard = {'1': '1' , '2': '2' , '3': '3' ,
             '7': '7' , '8': '8' , '9': '9' }
 
 def zero_to_infinity():
-    j = 0
-    while True:
-        yield j
-        j += 1
+	j = 0
+	while True:
+		yield j
+		j += 1
 
 def printBoard(board):
-  print(board['1'] + '|' + board['2'] + '|' + board['3'])
-  print('-+-+-')
-  print(board['4'] + '|' + board['5'] + '|' + board['6'])
-  print('-+-+-')
-  print(board['7'] + '|' + board['8'] + '|' + board['9'])
+	print(board['1'] + '|' + board['2'] + '|' + board['3'])
+	print('-+-+-')
+	print(board['4'] + '|' + board['5'] + '|' + board['6'])
+	print('-+-+-')
+	print(board['7'] + '|' + board['8'] + '|' + board['9'])
 
-def move(turn):
-  if turn % 2 == 0:
-    player = "X"
-    print("It's your turn " + player)
-  elif turn % 2 != 0:
-    player = "O"
-    print("It's your turn " + player)
-  move_place = str(input())
-  if player == "X":
-    if theBoard[move_place] == "X" or theBoard[move_place] == "O":
-      print("That place already taken, you can not make this move.")
-      return 0
-    else:
-      theBoard[move_place] = "X"
-      return 1
-  elif player == "O":
-    if theBoard[move_place] == "X" or theBoard[move_place] == "O":
-      print("That place already taken, you can not make this move.")
-      return 0
-    else:
-      theBoard[move_place] = "O"
-      return 1
+def game(turn):
+	if turn % 2 == 0:
+		player = "X"
+		print("It's your turn " + player)
+		move_place = input('')
+		if move_place == theBoard[move_place]:
+			theBoard[move_place] = "X"
+			printBoard(theBoard)
+			return 0
+		else:
+			print("You can not make this move.")
+			printBoard(theBoard)
+			game(turn)
+	elif turn % 2 == 1:
+		player = "O"
+		print("It's your turn " + player)
+		move_place = input('')
+		if move_place == theBoard[move_place]:
+			theBoard[move_place] = "O"
+			printBoard(theBoard)
+			return 0
+		else:
+			print("You can not make this move.")
+			printBoard(theBoard)
+			game(turn)
 
-def winner_tie_message(turn):
-  case_1 = theBoard["1"] == theBoard["2"] == theBoard["3"]
-  case_2 = theBoard["4"] == theBoard["5"] == theBoard["6"]
-  case_3 = theBoard["7"] == theBoard["8"] == theBoard["9"]
-  case_4 = theBoard["1"] == theBoard["4"] == theBoard["7"]
-  case_5 = theBoard["2"] == theBoard["5"] == theBoard["8"]
-  case_6 = theBoard["3"] == theBoard["6"] == theBoard["9"]
-  case_7 = theBoard["1"] == theBoard["5"] == theBoard["9"]
-  case_8 = theBoard["3"] == theBoard["5"] == theBoard["7"]
-  if turn % 2 == 0:
-    player = "X"
-  elif turn % 2 != 0:
-    player = "O"
-  if case_1 or case_2 or case_3 or case_4 or case_5 or case_6 or case_7 or case_8:
-    printBoard(theBoard)
-    print(f"*** CONGRATULATIONS {player} WON! ***")
-    return 0
-  else:
-    return 1
-  
+def win_conditions(turn):
+	win_1 = theBoard["1"] == theBoard["2"] == theBoard["3"]
+	win_2 = theBoard["4"] == theBoard["5"] == theBoard["6"]
+	win_3 = theBoard["7"] == theBoard["8"] == theBoard["9"]
+	win_4 = theBoard["1"] == theBoard["4"] == theBoard["7"]
+	win_5 = theBoard["2"] == theBoard["5"] == theBoard["8"]
+	win_6 = theBoard["3"] == theBoard["6"] == theBoard["9"]
+	win_7 = theBoard["1"] == theBoard["5"] == theBoard["9"]
+	win_8 = theBoard["3"] == theBoard["5"] == theBoard["7"]
+	if turn % 2 == 0 and turn != 8:
+		player = "X"
+		if win_1 or win_2 or win_3 or win_4 or win_5 or win_6 or win_7 or win_8:
+			print(f"*** CONGRATULATIONS {player} WON! ***")
+			return 0
+	elif turn % 2 == 1:
+		player = "Y"
+		if win_1 or win_2 or win_3 or win_4 or win_5 or win_6 or win_7 or win_8:
+			print(f"*** CONGRATULATIONS {player} WON! ***")
+			return 0
+	elif turn == 8:
+		print("*** IT'S A TIE! ***")
+		return 0
+
 def game_main():
-  for i in zero_to_infinity():
-    printBoard(theBoard)
-    while True:
-      if move(i) == 1:
-        break
-      elif move(i) == 0:
-        continue
-    if winner_tie_message(i) == 0:
-      break
-    elif i == 8:
-      printBoard(theBoard)
-      print("*** IT'S A TIE! ***")
-      break
+	printBoard(theBoard)
+	for i in range(9):
+		if game(i) == 0:
+			if win_conditions(i) == 0:
+				break
+			else:
+				continue
 
 if __name__ == "__main__":
-  game_main()
+	game_main()
